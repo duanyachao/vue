23451f020bcd45acafc943b8873a489e00c033b8
duanyachao <xuanyingsuixing@163.com>
2018-10-13 10:28:17 +0800

    vue demo

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div id="app">
+    <el-button type="warning">22222</el-button>
+    <el-button type="warning">222222222222</el-button>
+    <div class="nav">
+      <ul>
+          <li><router-link to="/">登录</router-link></li>
+          <li><router-link to="/main">首页</router-link></li>
+        </ul>
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
